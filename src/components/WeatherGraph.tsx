aec0bbf312527fa25b7a54e0cb05a6aee2449c99
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-03-28 21:35:01 +0530

    update

--- src/components/WeatherGraph.tsx
+++ src/components/WeatherGraph.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 
-import { LineChart, Line, ResponsiveContainer, CartesianGrid } from "recharts";
+import {
+  LineChart,
+  Line,
+  ResponsiveContainer,
+  CartesianGrid,
+  XAxis,
+  LabelProps,
+} from "recharts";
 
 const data = [
+  {
+    time: "Yesterday",
+    temp: 20,
+  },
   {
     time: "Morning",
     temp: 20,
@@ -19,58 +30,73 @@ const data = [
     time: "Night",
     temp: 42,
   },
+  {
+    time: "tomorrow",
+    temp: 30,
+  },
 ];
 
 function WeatherGraph() {
   return (
-    <div>
-      <div className="flex justify-between items-center">
-        <h3 className="text-2xl font-medium">Today's Temperature</h3>
-      </div>
-      <div className="flex pb-4 gap-6">
+    <div className="flex pb-4 justify-between max-h-[400px] flex-1">
+      <div className="w-[65%] flex flex-col">
+        {/* graph heading */}
+        <div className="flex justify-between items-center mb-4">
+          <h3 className="text-2xl font-medium">Today's Temperature</h3>
+        </div>
+
         {/* graph */}
-        <div className="w-[65%]">
-          <ResponsiveContainer height={200}>
-            <LineChart data={data}>
-              <CartesianGrid
-                horizontal={false}
-                strokeWidth={1}
-                stroke="#ededed"
-              />
-              <Line
-                type="monotone"
-                dataKey="temp"
-                stroke="#ff9124"
-                strokeWidth={1.5}
-                dot={{
-                  fill: "#ff9124",
-                  stroke: "#ff9124",
-                  strokeWidth: 1.2,
-                  r: 4,
-                }}
-              />
-            </LineChart>
-          </ResponsiveContainer>
-          <div className="flex justify-between py-3">
-            {data.map(({ time, temp }) => {
-              return <XAxisLabel temp={temp} time={time} />;
-            })}
+        <div className="w-full h-full flex justify-center relative overflow-hidden">
+          {/* line-graph and axis */}
+          <div className="h-full w-[135%] flex flex-col absolute">
+            <ResponsiveContainer width={"100%"}>
+              <LineChart data={data}>
+                <CartesianGrid
+                  horizontal={false}
+                  strokeWidth={1}
+                  stroke="#ededed"
+                />
+                <Line
+                  type="monotone"
+                  dataKey="temp"
+                  stroke="#ff9124"
+                  strokeWidth={1.5}
+                  dot={{
+                    fill: "#ff9124",
+                    stroke: "#ff9124",
+                    strokeWidth: 1.2,
+                    r: 4,
+                  }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+            <div className="flex justify-between">
+              {data.map(({ time, temp }) => {
+                return <XAxisLabel temp={temp} time={time} />;
+              })}
+            </div>
           </div>
+          {/* side smooth fades */}
+          <div className="w-[20px] absolute right-0 top-0 bottom-0 bg-gradient-to-r from-transparent to-slate-50"></div>
+          <div className="w-[20px] absolute left-0 top-0 bottom-0 bg-gradient-to-r from-slate-50 to-transparent"></div>
         </div>
-
-        {/* tomorrow */}
-        <div className="w-[35%] rounded-2xl bg-white shadow-lg">
-
-        </div>
-
       </div>
+
+      {/* tomorrow */}
+      <div className="w-[30%] rounded-2xl bg-[url('/weather/rain.jpg')] bg-cover"></div>
     </div>
   );
 }
 
-const XAxisLabel = ({ temp, time }) => {
+
+type XAxisLabelProps = {
+  temp: string,
+  time: string,
+} 
+
+const XAxisLabel = ({ temp, time } : XAxisLabelProps) => {
   return (
-    <div className="flex flex-col items-center gap-2">
+    <div className="flex flex-col items-center gap-2 last-of-type:invisible first-of-type:invisible">
       <span className="text-xl font-medium text-slate-800"> {`${temp}°`} </span>
       <span className="text-slate-400"> {time} </span>
     </div>
--- src/components/WeatherStats.tsx
+++ src/components/WeatherStats.tsx
@@ -1,34 +1,37 @@
 import React from "react";
+import { Image } from "react-feather";
 import thunderstorm from "@/assets/images/thunderstorm.png";
 import { HeavyRain } from "iconoir-react";
 import { Wind, Cloud } from "react-feather";
 
+import myImage from "../assets/images/cloud.png";
+
 function WeatherStats() {
   return (
-    <div className="flex gap-6 flex-1">
+    <div className="h-[350px] flex gap-6">
       {/* temperature */}
-      <div className="w-1/2 flex flex-col justify-between rounded-2xl p-6 bg-white shadow-lg shadow-slate-100">
+      <div className="w-1/2 flex flex-col justify-between rounded-2xl p-6 shadow-slate-100 bg-[url('/weather/sky.png')]">
         <div className="flex justify-between items-center">
-          <span className="p-3 rounded-full bg-orange-600 text-white">
+          <span className="p-3 rounded-full bg-white text-orange-500">
             <Cloud size={22} strokeWidth={1.5} />
           </span>
         </div>
 
         <div className="flex flex-col gap-2">
           <span className="text-4xl font-medium"> 34°C </span>
-          <span className="text-slate-500"> Partly Cloudy </span>
+          <span className=""> Partly Cloudy </span>
         </div>
 
         <div className="flex gap-3 leading-none">
-          <div className="w-1/3 flex flex-col gap-2 justify-center items-center bg-slate-100 rounded-xl p-5">
+          <div className="w-1/3 flex flex-col gap-2 justify-center items-center bg-slate-900 text-slate-50 rounded-xl p-5">
             <span> Pressure </span>
             <span className="text-xl font-medium"> 800mb </span>
           </div>
-          <div className="w-1/3 flex flex-col gap-2 items-center bg-slate-100 rounded-xl p-5">
+          <div className="w-1/3 flex flex-col gap-2 items-center bg-yellow-200 rounded-xl p-5">
             <span> Visibility </span>
             <span className="text-xl font-medium"> 4.3 km </span>
           </div>
-          <div className="w-1/3 flex flex-col gap-2 items-center bg-slate-100 rounded-xl p-5">
+          <div className="w-1/3 flex flex-col gap-2 items-center bg-white rounded-xl p-5">
             <span> Humidity </span>
             <span className="text-xl font-medium"> 87% </span>
           </div>
@@ -36,25 +39,25 @@ function WeatherStats() {
       </div>
 
       {/* wind and air quality */}
-      <div className="w-1/2 flex flex-col justify-between rounded-2xl p-6 bg-white shadow-lg shadow-slate-100">
+      <div className="w-1/2 flex flex-col justify-between rounded-2xl p-6 bg-white shadow-lg shadow-slate-100  bg-[url('/weather/clouds.jpg')] bg-cover">
         <div className="flex justify-between items-center">
-          <span className="p-3 rounded-full bg-orange-600 text-white">
+          <span className="p-3 rounded-full bg-white text-orange-500">
             <Wind size={22} strokeWidth={1.5} />
           </span>
         </div>
         <div className="flex flex-col gap-2">
           <span className="text-4xl font-medium"> 82 </span>
-          <span className="text-slate-500"> North West wind </span>
+          <span className=""> North West wind </span>
         </div>
-        <div className="flex flex-col gap-5 items-center bg-slate-100 rounded-xl p-5">
+        <div className="flex flex-col gap-5 items-center bg-white rounded-xl p-5">
           <div className="w-full flex justify-between leading-none">
-             <span> Good </span>
-             <span> Hazardous </span>
+            <span> Good </span>
+            <span> Hazardous </span>
           </div>
           {/* meter */}
           <div className="w-full h-[10px] rounded-lg bg-slate-200 relative">
             <span className="absolute left-0 top-0 bottom-0 w-[35%] rounded-lg bg-green-600">
-              {" "}
+
             </span>
           </div>
         </div>
