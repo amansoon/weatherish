b53b2e8496bcfba2cbb13fd7b334c64e1b6a232e
Aman Ghanghoriya <121393968+amonsoon@users.noreply.github.com>
2023-03-29 12:45:39 +0530

    update

--- src/components/WeatherGraph.tsx
+++ src/components/WeatherGraph.tsx
@@ -6,33 +6,36 @@ import {
   ResponsiveContainer,
   CartesianGrid,
   XAxis,
-  LabelProps,
+  YAxis,
+  ReferenceLine,
+  DotProps,
+  XAxisProps,
 } from "recharts";
 
 const data = [
   {
     time: "Yesterday",
-    temp: 20,
+    temp: 40,
   },
   {
     time: "Morning",
-    temp: 20,
+    temp: 35,
   },
   {
     time: "Afternoon",
-    temp: 34,
+    temp: 85,
   },
   {
     time: "Evening",
-    temp: 28,
+    temp: 30,
   },
   {
     time: "Night",
-    temp: 42,
+    temp: 12,
   },
   {
     time: "tomorrow",
-    temp: 30,
+    temp: 28,
   },
 ];
 
@@ -46,13 +49,14 @@ function WeatherGraph() {
         </div>
 
         {/* graph */}
-        <div className="w-full h-full flex justify-center relative overflow-hidden">
+        <div className="w-full h-full flex justify-center relative overflow-x-hidden">
           {/* line-graph and axis */}
           <div className="h-full w-[135%] flex flex-col gap-2 absolute">
             <ResponsiveContainer width={"100%"}>
-              <LineChart data={data}>
+              <LineChart data={data}  margin={{ top: 50 }}>
                 <CartesianGrid
                   horizontal={false}
+                  vertical={false}
                   strokeWidth={1.5}
                   stroke="#efefef"
                 />
@@ -78,7 +82,7 @@ function WeatherGraph() {
             </div>
           </div>
           {/* side smooth fades */}
-          <div className="w-full h-[20px] absolute right-0 left-0 top-0 bg-gradient-to-b from-slate-50 to-transparent"></div>
+          <div className="w-full h-[0px] absolute right-0 left-0 top-0 bg-gradient-to-b from-slate-50 to-transparent"></div>
           <div className="w-[50px] absolute right-0 top-0 bottom-0 bg-gradient-to-r from-transparent to-slate-50"></div>
           <div className="w-[50px] absolute left-0 top-0 bottom-0 bg-gradient-to-r from-slate-50 to-transparent"></div>
         </div>
@@ -108,107 +112,132 @@ const XAxisLabel = ({ temp, time }: XAxisLabelProps) => {
   return (
     <div className="w-[1px] first-of-type:invisible last-of-type:invisible">
       <div className="h-full w-full flex flex-col items-center gap-2">
-        <span className="text-xl font-medium text-slate-800">
-          {" "}
-          {`${temp}°`}
-        </span>
+        <span className="text-xl font-medium text-slate-800">{`${temp}°`}</span>
         <span className="text-slate-400"> {time} </span>
       </div>
     </div>
   );
 };
 
-class CustomizedLabel extends React.Component {
-  render() {
-    const { x, y, stroke, value } = this.props;
+// Customized label
 
-    return (
-      // <text
-      //   x={x}
-      //   y={y}
-      //   dy={-30}
-      //   fill={stroke}
-      //   textAnchor="middle"
-      //   className="text-normal text-rose-500"
-      // >
-      //   {value}
-      // </text>
+const CustomizedLabel = (props: DotProps) => {
+  const { x, y, stroke } = props;
 
-      <svg
-        x={x}
-        y={y}
-        width="42"
-        height="42"
-        viewBox="0 0 42 42"
-        fill="white"
-        xmlns="http://www.w3.org/2000/svg"
-        style={{backgroundColor: 'red'}}
-      >
-        <circle cx="21" cy="21" r="20.5" stroke="#C2D3FF" />
+  console.log(props);
+
+  return (
+    <svg
+      x={Number(x) - 25}
+      y={Number(y) - 60}
+      width="50"
+      height="51"
+      viewBox="0 0 50 51"
+      fill="white"
+      xmlns="http://www.w3.org/2000/svg"
+      style={{ backgroundColor: "red" }}
+    >
+      <g filter="url(#filter0_d_412_1253)">
+        <circle cx="25" cy="21" r="20.5" fill="white" stroke="#C2D3FF" />
         <g clip-path="url(#clip0_412_1253)">
           <path
-            d="M27.5334 25.557C28.3919 25.181 29.0949 24.5211 29.5245 23.6881C29.954 22.8551 30.0839 21.8996 29.8924 20.9822C29.7008 20.0647 29.1995 19.2411 28.4726 18.6495C27.7456 18.0579 26.8373 17.7344 25.9 17.7333H24.871C24.6121 16.7309 24.1187 15.8043 23.4315 15.03C22.7442 14.2557 21.8828 13.6558 20.9182 13.2797C19.9535 12.9037 18.9134 12.7623 17.8834 12.8672C16.8534 12.9721 15.863 13.3202 14.994 13.883C14.125 14.4457 13.4021 15.207 12.885 16.1039C12.368 17.0009 12.0715 18.0079 12.02 19.0419C11.9685 20.076 12.1634 21.1074 12.5888 22.0513C13.0142 22.9952 13.6579 23.8245 14.4667 24.4708"
-            stroke="black"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M17.7333 24.2667H17.7424"
+            d="M31.5334 25.557C32.3919 25.181 33.0949 24.5211 33.5245 23.6881C33.954 22.8551 34.0839 21.8996 33.8924 20.9822C33.7008 20.0647 33.1995 19.2411 32.4726 18.6495C31.7456 18.0579 30.8373 17.7344 29.9 17.7333H28.871C28.6121 16.7309 28.1187 15.8043 27.4315 15.03C26.7442 14.2557 25.8828 13.6558 24.9182 13.2797C23.9535 12.9037 22.9134 12.7623 21.8834 12.8672C20.8534 12.9721 19.863 13.3202 18.994 13.883C18.125 14.4457 17.4021 15.207 16.885 16.1039C16.368 17.0009 16.0715 18.0079 16.02 19.0419C15.9685 20.076 16.1634 21.1074 16.5888 22.0513C17.0142 22.9952 17.6579 23.8245 18.4667 24.4708"
             stroke="black"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
           <path
-            d="M17.7333 27.5333H17.7424"
+            d="M21.7333 24.2667H21.7424"
             stroke="black"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
           <path
-            d="M21 25.9H21.0091"
+            d="M21.7333 27.5333H21.7424"
             stroke="black"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
           <path
-            d="M21 29.1667H21.0091"
+            d="M25 25.9H25.0091"
             stroke="black"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
           <path
-            d="M24.2667 24.2667H24.2758"
+            d="M25 29.1667H25.0091"
             stroke="black"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
           <path
-            d="M24.2667 27.5333H24.2758"
+            d="M28.2667 24.2667H28.2758"
+            stroke="black"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M28.2667 27.5333H28.2758"
             stroke="black"
             stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
           />
         </g>
-        <defs>
-          <clipPath id="clip0_412_1253">
-            <rect
-              width="19.6"
-              height="19.6"
-              fill="white"
-              transform="translate(11.2 11.2)"
-            />
-          </clipPath>
-        </defs>
-      </svg>
-    );
-  }
-}
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_412_1253"
+          x="0"
+          y="0"
+          width="50"
+          height="51"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="5" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.709167 0 0 0 0 0.709167 0 0 0 0 0.709167 0 0 0 0.33 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_412_1253"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_412_1253"
+            result="shape"
+          />
+        </filter>
+        <clipPath id="clip0_412_1253">
+          <rect
+            width="19.6"
+            height="19.6"
+            fill="white"
+            transform="translate(15.2 11.2)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
 
 export default WeatherGraph;
